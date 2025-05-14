aea02683691ac515e408fb25c96ada36b280a1b8
Franklin Shera <fshera96@gmail.com>
2025-05-14 03:51:13 +0300

    add footer

--- app/components/TheFooter.tsx
+++ app/components/TheFooter.tsx
@@ -1,7 +1,14 @@
 export default function TheFooter() {
   return (
     <footer className="text-foreground/60 py-8 text-center text-sm">
-      <div className="container mx-auto">
+      <div className="container mx-auto space-y-2">
+        <p>
+          Vibe coded by{" "}
+          <a target="_blank" href="https://github.com/vfshera" className="underline">
+            Franklin Shera
+          </a>
+        </p>
+
         <p>© {new Date().getFullYear()} ScreenCapture. All rights reserved.</p>
       </div>
     </footer>
