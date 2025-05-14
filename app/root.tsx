1dc56a1e82886b599fc918b4e233e74d69bcee7f
Franklin Shera <fshera96@gmail.com>
2025-05-14 03:44:37 +0300

    add root

--- app/root.tsx
+++ app/root.tsx
@@ -1,14 +1,15 @@
+import type { Route } from "./+types/root";
 import {
-  isRouteErrorResponse,
   Links,
   Meta,
   Outlet,
   Scripts,
   ScrollRestoration,
+  isRouteErrorResponse,
 } from "react-router";
-
-import type { Route } from "./+types/root";
 import "./app.css";
+import TheNavbar from "./components/TheNavbar";
+import { Toaster } from "sonner";
 
 export const links: Route.LinksFunction = () => [
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
@@ -32,8 +33,12 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Meta />
         <Links />
       </head>
-      <body>
-        {children}
+      <body className="from-background to-muted bg-gradient-to-b">
+        <div className="min-h-screen">
+          <TheNavbar />
+          {children}
+        </div>
+        <Toaster />
         <ScrollRestoration />
         <Scripts />
       </body>
@@ -47,26 +52,26 @@ export default function App() {
 
 export function ErrorBoundary({ error }: Route.ErrorBoundaryProps) {
   let message = "Oops!";
+
   let details = "An unexpected error occurred.";
+
   let stack: string | undefined;
 
   if (isRouteErrorResponse(error)) {
     message = error.status === 404 ? "404" : "Error";
     details =
-      error.status === 404
-        ? "The requested page could not be found."
-        : error.statusText || details;
+      error.status === 404 ? "The requested page could not be found." : error.statusText || details;
   } else if (import.meta.env.DEV && error && error instanceof Error) {
     details = error.message;
     stack = error.stack;
   }
 
   return (
-    <main className="pt-16 p-4 container mx-auto">
+    <main className="container mx-auto p-4 pt-16">
       <h1>{message}</h1>
       <p>{details}</p>
       {stack && (
-        <pre className="w-full p-4 overflow-x-auto">
+        <pre className="w-full overflow-x-auto p-4">
           <code>{stack}</code>
         </pre>
       )}
